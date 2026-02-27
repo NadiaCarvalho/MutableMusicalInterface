d8c0bb6b897f83313a48f8cc90a1ad19b649a197
NadiaCarvalho <nadiacarvalho118@gmail.com>
2026-02-27 15:13:18 +0000

    starting website

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './style.css' // Ensure your Tailwind/CSS imports are here
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
